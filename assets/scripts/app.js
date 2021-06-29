752c7f9da02a3ba0e0eaaafc6dba20af374f2f6b
mjmorrison10 <mjmorrison10@hotmail.com>
2021-06-28 20:58:21 -0700

    first commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -41,7 +41,6 @@ const Portfolio = function() {
 		var count = 1;
 		$(window).on('scroll', function() {
 			var y_scroll_pos = window.pageYOffset;
-			var scroll_pos_test = 2700; // set to whatever you want it to be
 			var words = makeWords();
 			if (y_scroll_pos > scroll_pos_test && count <= 1) {
 				makeWordCloud(words);
@@ -57,15 +56,15 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a beginner Front-End Web Developer.", "love creating with code!", "also teach programming to people.", "enjoy solving problems problems."
+				"am a beginner Front-End Web Developer.", "love creating with code!", "also teach programming to people.", "enjoy solving problems problems.", "and a proud American!"
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
 			// typing speed
-			typeSpeed: 1,
+			typeSpeed: 20,
 			contentType: 'text',
 			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+				$("#writing-text").css({"color": "#3F4045", "background-color": "#ffffff"});
 			},
 			preStringTyped: function() {},
 			onStringTyped: function() {}
