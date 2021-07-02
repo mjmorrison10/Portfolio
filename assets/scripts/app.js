159e7c8133a38d6a56edbdd0439f5a64f64bfee7
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-07-01 23:16:31 -0700

    QoL update
    Qol

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -45,7 +45,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a beginner Front-End Web Developer.", "love creating with code!", "also teach programming to people.", "enjoy solving problems problems.", "am proud American!"
+				"am a beginner Front-End Web Developer.", "love creating with code!", "also teach programming to people.", "enjoy solving problems problems.", "am a proud American!"
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
